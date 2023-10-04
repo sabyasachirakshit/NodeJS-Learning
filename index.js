5eb8802d9131731fb2012c5cc63566656bea972a
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2023-10-04 19:59:16 +0530

    GET API endpoint made

--- index.js
+++ index.js
@@ -4,9 +4,26 @@ const hostname = "127.0.0.1";
 const port = 8000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World!");
+  res.setHeader("Content-Type", "application/json");
+
+  // Handle GET requests to the /api endpoint
+  if (req.method === "GET" && req.url === "/api") {
+    const responseObj = { message: "This is the API endpoint" };
+    res.statusCode = 200;
+    res.end(JSON.stringify(responseObj));
+  } else if (req.method === "GET" && req.url === "/api/deep-api") {
+    const responseObj = { message: "This is the deep API endpoint" };
+    res.statusCode = 200;
+    res.end(JSON.stringify(responseObj));
+  } else {
+    // Handle other requests with a 404 Not Found response
+    res.statusCode = 404;
+    res.end(JSON.stringify({ error: "Not Found" }));
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 server.listen(port || 8000, hostname || "127.0.0.1", () => {
