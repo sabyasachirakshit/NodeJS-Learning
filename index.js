42d1cd44539a72d9f8d5252eb65fde0c7a750122
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2023-10-04 20:11:02 +0530

    post api call working

--- index.js
+++ index.js
@@ -15,6 +15,31 @@ const server = http.createServer((req, res) => {
     const responseObj = { message: "This is the deep API endpoint" };
     res.statusCode = 200;
     res.end(JSON.stringify(responseObj));
+  } else if (req.method === "POST" && req.url === "/api/post") {
+    // Handle POST requests to the /api endpoint
+    let requestBody = "";
+
+    // Listen for data events to collect the request body
+    req.on("data", (chunk) => {
+      requestBody += chunk.toString();
+      console.log(requestBody);
+    });
+
+    // When the request is complete, parse and respond with the data
+    req.on("end", () => {
+      try {
+        const recievedData = JSON.parse(requestBody);
+        const responseData = {
+          message: "This is the API endpoint (POST)",
+          data: recievedData,
+        };
+        res.statusCode = 200;
+        res.end(JSON.stringify(responseData));
+      } catch (error) {
+        res.statusCode = 400; // Bad Request
+        res.end(JSON.stringify({ error: "Invalid JSON data" }));
+      }
+    });
   } else {
     // Handle other requests with a 404 Not Found response
     res.statusCode = 404;
