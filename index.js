ff2ef1c954afcf2a25fa04f6818e39014c77de30
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2023-10-05 11:53:39 +0530

    PATCH API created

--- index.js
+++ index.js
@@ -83,8 +83,7 @@ const server = http.createServer((req, res) => {
         }
       } catch (error) {
         res.statusCode = 400; // Bad Request
-        console.log("This is error:", error);
-        res.end(JSON.stringify({ error: "Invalid JSON data" }));
+        res.end(JSON.stringify({ error: error }));
       }
     });
   } else if (req.method === "PUT" && req.url === "/sr-api") {
@@ -146,8 +145,63 @@ const server = http.createServer((req, res) => {
         }
       } catch (error) {
         res.statusCode = 400; // Bad Request
-        console.log("This is error:", error);
-        res.end(JSON.stringify({ error: "Invalid JSON data" }));
+        res.end(JSON.stringify({ error: error }));
+      }
+    });
+  } else if (req.method === "PATCH" && req.url === "/sr-api") {
+    // Handle PATCH requests to the /api endpoint
+    let requestBody = "";
+
+    // Listen for data events to collect the request body
+    req.on("data", (chunk) => {
+      requestBody += chunk.toString();
+    });
+
+    // When the request is complete, parse and respond with the data
+    req.on("end", () => {
+      try {
+        let flag = 0;
+        const recievedData = JSON.parse(requestBody);
+        console.log("This is the data received:", recievedData);
+        function isValidData(data) {
+          // Check if 'data' is an object
+          if (typeof data === "object" && data !== null) {
+            // Check if it has 'name' and 'age' properties
+            if (data.hasOwnProperty("name") && data.hasOwnProperty("age")) {
+              return true;
+            }
+          }
+          return false;
+        }
+
+        if (isValidData(recievedData)) {
+          let matchingObject = database.find(
+            (item) => item.name === recievedData.name
+          );
+          if (matchingObject) {
+            matchingObject.age = recievedData.age;
+            flag = 1;
+          }
+          if (flag === 0) {
+            res.statusCode = 400; // Bad Request
+            res.end(JSON.stringify({ error: "Data does not exsits" }));
+          } else {
+            const responseData = {
+              message:
+                "This is the API endpoint (PATCH), changes made successfully!",
+              data: database,
+            };
+            res.statusCode = 200;
+            res.end(JSON.stringify(responseData));
+          }
+        } else {
+          res.statusCode = 400; // Bad Request
+          console.log("This is error:", error);
+          res.end(JSON.stringify({ error: "Invalid JSON data" }));
+        }
+      } catch (error) {
+        res.statusCode = 400; // Bad Request
+        res.end(JSON.stringify({ error: error }));
       }
     });
   } else if (req.method === "DELETE" && req.url === "/sr-api") {
@@ -206,7 +260,7 @@ const server = http.createServer((req, res) => {
       } catch (error) {
         res.statusCode = 400; // Bad Request
         console.log("This is error:", error);
-        res.end(JSON.stringify({ error: "Invalid JSON data" }));
+        res.end(JSON.stringify({ error: error }));
       }
     });
   } else {
