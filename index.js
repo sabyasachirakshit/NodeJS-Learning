a1eff1b621c701ffe3560d5017ae4e54c17dc7d3
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2023-10-05 11:17:50 +0530

    POST api call validating json parameters

--- index.js
+++ index.js
@@ -26,6 +26,50 @@ const server = http.createServer((req, res) => {
     res.end(JSON.stringify(responseObj));
   } else if (req.method === "POST" && req.url === "/sr-api") {
     //Handle POST requests to the /api endpoint
+    let requestBody = "";
+
+    // Listen for data events to collect the request body
+    req.on("data", (chunk) => {
+      requestBody += chunk.toString();
+    });
+
+    // When the request is complete, parse and respond with the data
+    req.on("end", () => {
+      try {
+        const recievedData = JSON.parse(requestBody);
+        console.log("This is the data received:", recievedData);
+        function isValidData(data) {
+          // Check if 'data' is an object
+          if (typeof data === "object" && data !== null) {
+            // Check if it has 'name' and 'age' properties
+            if (data.hasOwnProperty("name") && data.hasOwnProperty("age")) {
+              return true;
+            }
+          }
+          return false;
+        }
+        if (isValidData(recievedData)) {
+          database.push(recievedData);
+          const responseData = {
+            message:
+              "This is the API endpoint (POST), changes made successfully!",
+            data: database,
+          };
+          res.statusCode = 200;
+          res.end(JSON.stringify(responseData));
+        } else {
+          const responseData = {
+            message: "Invalid JSON parameters",
+          };
+          res.statusCode = 400;
+          res.end(JSON.stringify(responseData));
+        }
+      } catch (error) {
+        res.statusCode = 400; // Bad Request
+        console.log("This is error:", error);
+        res.end(JSON.stringify({ error: "Invalid JSON data" }));
+      }
+    });
   } else if (req.method === "PUT" && req.url === "/sr-api") {
     // Handle PUT requests to the /api endpoint
     let requestBody = "";
@@ -41,28 +85,45 @@ const server = http.createServer((req, res) => {
         let flag = 0;
         const recievedData = JSON.parse(requestBody);
         console.log("This is the data received:", recievedData);
-        let matchingObject = database.find(
-          (item) => item.name === recievedData.name
-        );
-        if (matchingObject) {
-          matchingObject.age = recievedData.age;
-          flag = 1;
+        function isValidData(data) {
+          // Check if 'data' is an object
+          if (typeof data === "object" && data !== null) {
+            // Check if it has 'name' and 'age' properties
+            if (data.hasOwnProperty("name") && data.hasOwnProperty("age")) {
+              return true;
+            }
+          }
+          return false;
         }
-        if (flag === 0) {
-          res.statusCode = 400;
-          const responseData = {
-            message: "Bad Request",
-            type: "could not find user in database",
-          };
-          res.end(JSON.stringify(responseData));
+
+        if (isValidData(recievedData)) {
+          let matchingObject = database.find(
+            (item) => item.name === recievedData.name
+          );
+          if (matchingObject) {
+            matchingObject.age = recievedData.age;
+            flag = 1;
+          }
+          if (flag === 0) {
+            res.statusCode = 400;
+            const responseData = {
+              message: "Bad Request",
+              type: "could not find user in database",
+            };
+            res.end(JSON.stringify(responseData));
+          } else {
+            const responseData = {
+              message:
+                "This is the API endpoint (PUT), changes made successfully!",
+              data: database,
+            };
+            res.statusCode = 200;
+            res.end(JSON.stringify(responseData));
+          }
         } else {
-          const responseData = {
-            message:
-              "This is the API endpoint (PUT), changes made successfully!",
-            data: database,
-          };
-          res.statusCode = 200;
-          res.end(JSON.stringify(responseData));
+          res.statusCode = 400; // Bad Request
+          console.log("This is error:", error);
+          res.end(JSON.stringify({ error: "Invalid JSON data" }));
         }
       } catch (error) {
         res.statusCode = 400; // Bad Request
